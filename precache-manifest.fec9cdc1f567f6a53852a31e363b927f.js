9aa548d75c06d9a26315ecfd6c14a6ebd24fc2b3
Rahul Rakshit <rahul.rakshit@posteo.net>
2019-07-09 15:02:46 +0100

    Updates

---
+++ precache-manifest.fec9cdc1f567f6a53852a31e363b927f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a51783a805abf28b1c3ba53376da1f",
+    "url": "/main/index.html"
+  },
+  {
+    "revision": "01d0d0cef1e95759b4f9",
+    "url": "/main/static/js/2.14aa8d28.chunk.js"
+  },
+  {
+    "revision": "9899d9c4b38fa9e63d03",
+    "url": "/main/static/js/main.f1859182.chunk.js"
+  },
+  {
+    "revision": "8971e5ec5c48ef29e288",
+    "url": "/main/static/js/runtime~main.7e08dea4.js"
+  }
+]);
